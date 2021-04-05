f0b98841a2f4b5c23ac967619886119079eef783
Dan Price <dan.price@chameleontechnology.co.uk>
2021-04-05 13:23:46 +0100

    handle input event in request handler

--- src/request_handler/request_handler.js
+++ src/request_handler/request_handler.js
@@ -1,5 +1,7 @@
 const axios = require("axios");
 
+const BASE_REQUEST_URL = `http://host.docker.internal:3000/providers/`;
+
 exports.handler = async function (event) {
   //just to test my localstack is set up correctly. Test using:
   //awslocal lambda invoke --function-name request_handler output.txt
@@ -9,4 +11,25 @@ exports.handler = async function (event) {
     "https://webhook.site/e9f6dbce-5b3e-468a-ba7f-de7e4a6caa68",
     { event }
   );
+
+  const messageAttributes = event.Records[0].messageAttributes;
+  const provider = messageAttributes.provider.stringValue;
+  const callbackUrl = messageAttributes.callbackUrl.stringValue;
+
+  try {
+    const response = await axios.get(`${BASE_REQUEST_URL}${provider}`);
+    const data = response.data;
+
+    await axios.post(
+      "https://webhook.site/e9f6dbce-5b3e-468a-ba7f-de7e4a6caa68",
+      { data }
+    );
+  } catch (e) {
+    //the server isn't available, so add it back to the queue to be tried again
+    //TODO: add delivery delay, with exponential backoff
+    await axios.post(
+      "https://webhook.site/e9f6dbce-5b3e-468a-ba7f-de7e4a6caa68",
+      { event: "got error", error: e }
+    );
+  }
 };
