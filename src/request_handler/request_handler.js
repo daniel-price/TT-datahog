5ed8e53179f3f47c29c40e267f8d3848f8584514
Dan Price <dan.price@chameleontechnology.co.uk>
2021-04-05 14:17:50 +0100

    add naive retry mechanism to request handler

--- src/request_handler/request_handler.js
+++ src/request_handler/request_handler.js
@@ -1,17 +1,10 @@
 const axios = require("axios");
+const { sendMessageToQueue } = require("./aws");
 
+const RETRY_QUEUE = "http://localhost:4566/000000000000/request_handler_queue";
 const BASE_REQUEST_URL = `http://host.docker.internal:3000/providers/`;
 
 exports.handler = async function (event) {
-  //just to test my localstack is set up correctly. Test using:
-  //awslocal lambda invoke --function-name request_handler output.txt
-  //Test SQS queue using:
-  //awslocal sqs send-message --queue-url http://localhost:4566/000000000000/request_handler_queue --message-body "test"
-  await axios.post(
-    "https://webhook.site/e9f6dbce-5b3e-468a-ba7f-de7e4a6caa68",
-    { event }
-  );
-
   const messageAttributes = event.Records[0].messageAttributes;
   const provider = messageAttributes.provider.stringValue;
   const callbackUrl = messageAttributes.callbackUrl.stringValue;
@@ -27,9 +20,8 @@ exports.handler = async function (event) {
   } catch (e) {
     //the server isn't available, so add it back to the queue to be tried again
     //TODO: add delivery delay, with exponential backoff
-    await axios.post(
-      "https://webhook.site/e9f6dbce-5b3e-468a-ba7f-de7e4a6caa68",
-      { event: "got error", error: e }
-    );
+    await sendMessageToQueue(RETRY_QUEUE, { provider, callbackUrl });
   }
+
+  return true;
 };
